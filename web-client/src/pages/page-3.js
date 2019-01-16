15d8b2319697d86b22f2e5a7901ecb322be810d0
ilyafefelov <ilyafefelov@gmail.com>
2019-01-16 22:08:40 +0200

    fixes

--- web-client/src/pages/page-3.js
+++ web-client/src/pages/page-3.js
@@ -10,37 +10,23 @@ const SecondPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Page two" />
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 3</p>
-      {/* {data.allFile.edges.map(({ node }, index) => (
-        <li key={index}>
-          <ul>
-            <img
-              style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}
-              src={node.publicURL}
-              alt="file"
-            />
-          </ul>
-          <ul>{node.prettySize}</ul>
-          <ul>{node.extension}</ul>
-          <ul>{node.birthTime}</ul>
-        </li>
-      ))} */}
-
+      <h1>Here are some Posts</h1>
+      <p>Welcome to good-ua selection</p>
+      
       {data.allStrapiPost.edges.map(({ node }, index) => (
-        <li key={index}>
-          <ul>
+        <div key={index}>
+          <div style={{background:"darksalmon", borderRadius:"3px"}}>
             {/* <img
               style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}
               src={node.publicURL}
               alt="file"
             /> */}
+            <ul style={{fontSize:"21px", fontFamily:'monospace', padding:"12px 0 6px 0"}} >{node.title}</ul>
             <ul>{node.id}</ul>
-            <ul>{node.title}</ul>
             <ul>{node.content}</ul>
 
-          </ul>
-        </li>
+          </div>
+        </div>
       ))}
       <Link to="/">Go back to the homepage</Link>
     </Layout>

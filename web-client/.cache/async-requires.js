8009e105465efd01d5d9328a22c558331e004583
ilyafefelov <ilyafefelov@gmail.com>
2019-01-22 23:06:07 +0200

    easy apoolo posts

--- web-client/.cache/async-requires.js
+++ web-client/.cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-app-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\app.js" /* webpackChunkName: "component---src-pages-app-js" */),
   "component---src-pages-index-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
--- web-client/.cache/sync-requires.js
+++ web-client/.cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\404.js"))),
   "component---src-pages-app-js": hot(preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\app.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\index.js"))),

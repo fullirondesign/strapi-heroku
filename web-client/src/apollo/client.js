eb5960a7999191649aea2572c970fefe8c7d61b2
full_iron <ilyafefelov@gmail.com>
2019-01-27 18:25:23 +0200

    Update client.js

--- web-client/src/apollo/client.js
+++ web-client/src/apollo/client.js
@@ -2,9 +2,9 @@ import ApolloClient from 'apollo-boost';
 // import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:1337/graphql',
-  // uri: 'http://good-ua.herokuapp.com/graphql', //TODO illya 
-  // fetch,
+//   uri: 'http://localhost:1337/graphql',
+  uri: 'http://good-ua.herokuapp.com/graphql', //TODO illya 
+  fetch,
   fetchOptions: {
     mode: 'cors',
   },

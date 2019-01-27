8828d83103809bd26077ab2a283252d43274b924
ilyafefelov <ilyafefelov@gmail.com>
2019-01-27 17:31:41 +0200

    try differnt client setting

--- web-client/src/apollo/client.js
+++ web-client/src/apollo/client.js
@@ -2,6 +2,7 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:1337/graphql',
+  // uri: 'http://localhost:1337/graphql',
+  uri: 'http://good-ua.herokuapp.com/graphql',
   fetch,
 });

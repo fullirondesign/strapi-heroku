b3902044d8e282e74c63dfdb208a9f10dcd825ae
ilyafefelov <ilyafefelov@gmail.com>
2019-01-28 12:26:47 +0200

    test - WORKING for now

--- web-client/.cache/sync-requires.js
+++ web-client/.cache/sync-requires.js
@@ -3,9 +3,10 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\.cache\\dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\index.js")),
   "component---src-pages-posts-1-js": preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\posts.1.js")),
-  "component---src-pages-posts-js": preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\posts.js")),
-  "component---src-pages-test-js": preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\test.js"))
+  "component---src-pages-test-js": preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\test.js")),
+  "component---src-pages-posts-3-js": preferDefault(require("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\posts3.js"))
 }
 
--- web-client/src/apollo/client.js
+++ web-client/src/apollo/client.js
@@ -1,11 +1,16 @@
 import ApolloClient from 'apollo-boost';
-// import fetch from 'isomorphic-fetch';
+import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
   uri: 'http://localhost:1337/graphql',
-  // uri: 'http://good-ua.herokuapp.com/graphql', //TODO illya 
-  // fetch,
-  fetchOptions: {
-    mode: 'cors',
-  },
+  
+  fetch
+
 });
+
+
+// uri: 'http://good-ua.herokuapp.com/graphql', //TODO illya 
+
+  // fetchOptions: {
+  //   mode: 'no-cors',
+  // },

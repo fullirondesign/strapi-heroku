7fda42f2e85ccf060125cea69e7d7c8fdce02807
ilyafefelov <ilyafefelov@gmail.com>
2019-01-28 12:32:10 +0200

    new for public - working on CORS

--- web-client/.cache/async-requires.js
+++ web-client/.cache/async-requires.js
@@ -2,11 +2,10 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-index-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-posts-1-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\posts.1.js" /* webpackChunkName: "component---src-pages-posts-1-js" */),
-  "component---src-pages-test-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\test.js" /* webpackChunkName: "component---src-pages-test-js" */),
-  "component---src-pages-posts-3-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\posts3.js" /* webpackChunkName: "component---src-pages-posts-3-js" */)
+  "component---src-pages-posts-3-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\posts3.js" /* webpackChunkName: "component---src-pages-posts-3-js" */),
+  "component---src-pages-test-js": () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\src\\pages\\test.js" /* webpackChunkName: "component---src-pages-test-js" */)
 }
 
 exports.data = () => import("C:\\Users\\ilyafefelov\\Dropbox\\Code\\strapi-heroku\\good-ua\\web-client\\.cache\\data.json")
